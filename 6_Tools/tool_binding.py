4efd57038a4a4c5a9c8476e60d4fa33aee67a009
unknown <sourav009966@gmail.com>
2026-08-21 23:49:17 +0530

    new addtion

--- 6_Tools/tool_binding.py
+++ 6_Tools/tool_binding.py
@@ -28,10 +28,47 @@ llm_with_tool = llm.bind_tools([get_text_length]) #here we wil provide all the a
 #so now we have 2 llm ,technically "llm" and "llm_with_tool" are same, but first llm not have tool and second have 
 
 result = llm.invoke("hello ")
+result2 = llm_with_tool.invoke("hello ")
 
 #print(result.content)
 
+#OUTPUT >> 
+
+
 #Hello! 😊 How can I assist you today?
  
+#______________________________________________________________________________________________________________________________________________________
+#removing .content >> 
 
 print(result)
+print()
+print()
+print()
+print(result2)
+
+#we are able to print like this as are using > "from rich import print"
+
+# AIMessage(
+#     content='Hello! 😊 How can I assist you today?',
+#     additional_kwargs={},
+#     response_metadata={
+#         'token_usage': {
+#             'prompt_tokens': 17,
+#             'total_tokens': 30,
+#             'completion_tokens': 13,
+#             'prompt_tokens_details': {'cached_tokens': 0},
+#             'service_tier': 'standard'
+#         },
+#         'model_name': 'mistral-small-2506',
+#         'model': 'mistral-small-2506',
+#         'finish_reason': 'stop',
+#         'model_provider': 'mistralai'
+#     },
+#     id='lc_run--01a02066-b7b6-7a90-bb0f-2ff9909c2ff5-0',
+#     tool_calls=[],
+#     invalid_tool_calls=[],
+#     usage_metadata={'input_tokens': 17, 'output_tokens': 13, 'total_tokens': 30}
+# )
+
+
+#______________________________________________________________________________________________________________________________________________________
